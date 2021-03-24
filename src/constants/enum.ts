a139ce2bd22436f1b6b883f299cfa99c6146f075
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-03-24 17:30:39 +0700

    Updates

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -20,8 +20,31 @@ export enum AuthRoles {
 }
 
 export enum Location {
-  HANOI = 'HAN',
-  HOCHIMINH = 'HCM',
-  DANANG = 'DAN',
-  QUANGNINH = 'QAN',
+  HANOI_SUBNAME = 'HAN',
+  HOCHIMINH_SUBNAME = 'HCM',
+  DANANG_SUBNAME = 'DAN',
+  QUANGNINH_SUBNAME = 'QAN',
+
+  HANOI = 'Hà Nội',
+  HOCHIMINH = 'Hồ Chí Minh',
+  DANANG = 'Đà Nẵng',
+  QUANGNINH = 'Quảng Ninh',
 }
+
+export enum DayOfWeek {
+  MONDAY = 1,
+  TUESDAY,
+  WEDNESDAY,
+  THURSDAY,
+  FRIDAY,
+  SATURDAY,
+  SUNDAY,
+}
+
+// MONDAY = 'Thứ Hai'
+// TUESDAY = 'Thứ Ba'
+// WEDNESDAY = 'Thứ Tư'
+// THURSDAY = 'Thứ Năm'
+// FRIDAY = 'Thứ Sáu'
+// SATURDAY = 'Thứ Bảy'
+// SUNDAY = 'Chủ Nhật'
