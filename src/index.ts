57b5b5c9f47fb57ab62b80949ca0cfafd4ef39d1
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-03-26 02:00:12 +0700

    Updates

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from './constants/payload';
 
 //* Events
 export * from './events/staff-created-event';
+export * from './events/driver-created-event';
 
 //* Middlewares
 export * from './middlewares/current-user.middleware';
