9bbda250ab23c7ac9f7994835c9b81240cfca281
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-04-19 02:02:55 +0700

    Updates

--- src/messages/transit-detail-creating-mess.ts
+++ src/messages/transit-detail-creating-mess.ts
@@ -1,6 +1,7 @@
 import { BookingStatus, Location, TripShift } from '../constants/enum';
 
 export interface TransitDetailCreatingMess {
+  bookingId: number;
   bookingName: string;
   bookingPhone: string;
   bookingStatus: BookingStatus;
