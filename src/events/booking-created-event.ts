278a7a4481e043e28a948be1204c593332143349
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-04-20 11:04:57 +0700

    Updates

--- src/events/booking-created-event.ts
+++ src/events/booking-created-event.ts
@@ -4,7 +4,13 @@ export interface BookingCreatedEvent {
   id: number;
   tripId: string;
   bookingName: string;
+  bookingMail: string;
   bookingPhone: string;
+  totalPrice: number;
+  hasTransit: boolean;
+  transitDetailId: number;
+  notes: string;
+  isVerify: boolean;
   bookingStatus: BookingStatus;
   paymentMethod: PaymentMethod;
   transitDetail: {
