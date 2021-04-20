3e9fb1129a714a9abe88b9657b3e960977eaca62
Dang Vu Nhat Quang <ghitacluber@gmail.com>
2021-04-20 11:07:39 +0700

    Updates

--- src/patterns/events.ts
+++ src/patterns/events.ts
@@ -5,6 +5,7 @@ export enum Events {
   TripStatusUpdatedDev = 'trip:status:updated:dev',
   TransitCreatedDev = 'transit:created:dev',
   TransitStatusUpdatedDev = 'transit:status:updated:dev',
+  BookingCreatedDev = 'booking:created:dev',
 
   StaffCreated = 'staff:created',
   DriverCreated = 'driver:created',
@@ -12,4 +13,5 @@ export enum Events {
   TripStatusUpdated = 'trip:status:updated',
   TransitCreated = 'transit:created',
   TransitStatusUpdated = 'transit:status:updated',
+  BookingCreated = 'booking:created',
 }
